43a5a6763339da7c6d0f1ce324be981560b102da
yudahadi14 <yudahadi14@gmail.com>
2020-01-15 10:49:41 +0700

    Updates

---
+++ precache-manifest.4f96d8e33fd509a505045b2bf798f100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3e465c25711dec17fee2a382974b66",
+    "url": "/myprojectt/index.html"
+  },
+  {
+    "revision": "d346b138268d04ff842a",
+    "url": "/myprojectt/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a38287d609fc91c7329",
+    "url": "/myprojectt/static/js/2.0773df18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myprojectt/static/js/2.0773df18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d346b138268d04ff842a",
+    "url": "/myprojectt/static/js/main.a17c8f81.chunk.js"
+  },
+  {
+    "revision": "7cebe7c4849154c3d8da",
+    "url": "/myprojectt/static/js/runtime-main.34da9e6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprojectt/static/media/logo.5d5d9eef.svg"
+  }
+]);
